eeed807a991c1e3103ae7503b8188233cadcacd5
Asad Khalid <akha0107@uni.sydney.edu.au>
2025-10-21 13:38:45 +1100

    Second commit

--- questions.py
+++ questions.py
@@ -1,21 +1,32 @@
-# TODO:
-# Implement the functions below
-
 import random
 
 
 def generate_mathematics_question() -> str:
-    pass
+    a = random.randint(1, 20)
+    b = random.randint(1, 20)
+    return f"{a} + {b}"
 
     
 def generate_roman_numerals_question() -> str:
     number = random.randint(1, 3999)
-    # Convert this number to a roman numeral!
+    vals = [
+        (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+        (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+        (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")
+    ]
+    roman = ""
+    for (decimal_value, numeral) in vals:
+        while number >= decimal_value:
+            roman += numeral
+            number -= decimal_value
+    return roman
 
     
 def generate_usable_addresses_question() -> str:
-    pass
+    subnets = ["192.168.1.0/24", "10.0.0.0/16", "172.16.0.0/20"]
+    return random.choice(subnets)
 
     
 def generate_network_broadcast_question() -> str:
-    pass
+    subnets = ["192.168.1.0/24", "10.0.0.0/16", "172.16.0.0/20"]
+    return random.choice(subnets)
