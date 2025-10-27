1d631f77d784037b3a48158183783e28ccc1c087
Asad Khalid <akha0107@uni.sydney.edu.au>
2025-10-27 14:24:31 +1100

    Bye message

--- client.py
+++ client.py
@@ -57,9 +57,16 @@ def main():
                     answer = input_handler_with_timeouts(message["time_limit"])
                     if answer is not None:
                         if answer == "EXIT":
-                            send_json(sock, {"message_type": "BYE"})
-                            sock.close()
-                            sys.exit(0)
+                            try:
+                                _ = question_queue.get(timeout=0.1)
+                                send_json(sock, {"message_type": "ANSWER", "answer": answer})
+                                send_json(sock, {"message_type": "BYE"})
+                                sock.close()
+                                sys.exit(0)
+                            except:
+                                send_json(sock, {"message_type": "BYE"})
+                                sock.close()
+                                sys.exit(0)
                         elif answer == "DISCONNECT":
                             send_json(sock, {"message_type": "BYE"})
                             sock.close()
